aed58c6f501493eab6c0c591987cdafca556d461
candra192 <abaycandra@gmail.com>
2025-06-02 23:10:53 +0700

    hapus fullscreen, rapiin galeri scroll vertical

--- script.js
+++
@@ -1,8 +0,0 @@
-function openModal(elem) {
-  document.getElementById("modal").style.display = "block";
-  document.getElementById("modal-img").src = elem.src;
-}
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
